7240266fdaf8cd016199ef82f20b895f46eb927e
Sweetha03 <121707446+Sweetha03@users.noreply.github.com>
2023-02-19 18:48:50 +0530

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AdminComponent } from './admin/admin.component';
+import { UserComponent } from './user/user.component';
+import { RouterModule,Routes } from '@angular/router';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AdminComponent,
+    UserComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    RouterModule.forRoot(routes),
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
